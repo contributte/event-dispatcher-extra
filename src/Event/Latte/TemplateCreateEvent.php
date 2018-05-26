0766daad6c87468251160a43102ed538a6b17aab
Marek Bartos <bartos.developer152@gmail.com>
2018-05-27 01:41:05 +0200

    Add TemplateCreateEvent

---
+++ src/Event/Latte/TemplateCreateEvent.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types = 1);
+
+namespace Contributte\Events\Extra\Event\Latte;
+
+use Nette\Application\UI\Control;
+use Nette\Bridges\ApplicationLatte\Template;
+use Symfony\Component\EventDispatcher\Event;
+
+class TemplateCreateEvent extends Event
+{
+
+	public const NAME = LatteEvents::ON_TEMPLATE_CREATE;
+
+	/** @var Template */
+	private $template;
+
+	/**@var Control */
+	private $control;
+
+	public function __construct(Template $template, Control $control)
+	{
+		$this->template = $template;
+		$this->control = $control;
+	}
+
+	public function getTemplate(): Template
+	{
+		return $this->template;
+	}
+
+	public function getControl(): Control
+	{
+		return $this->control;
+	}
+}
